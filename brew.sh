75b96324a2dfe2ba4d051f4ca1b397b99c967ff6
XhstormR <10527522+XhstormR@users.noreply.github.com>
2023-01-13 15:30:41 +0800

    Update

--- brew.sh
+++ brew.sh
@@ -28,15 +28,15 @@ brew install --cask itsycal
 brew install --cask karabiner-elements
 brew install --cask keepassxc
 brew install --cask maczip
-brew install --cask macfuse
 brew install --cask megasync
 brew install --cask miniconda
+brew install --cask macfuse
 brew install --cask mounty
 brew install --cask onyx
 brew install --cask qview
 brew install --cask rectangle
 brew install --cask stats
-brew install --cask snipaste
+brew install --cask shottr
 brew install --cask telegram
 brew install --cask vlc
 
