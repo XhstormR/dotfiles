e24dcd637bfd823382e910c5764256df7f465a85
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-06-19 16:44:32 +0300

    Update

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,9 @@
 fish_config theme choose Dracula
 
-fish_add_path /usr/bin
 fish_add_path /usr/local/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.pixi/bin
-fish_add_path ~/.pixi/envs/openjdk/lib/jvm/bin
 
 eval fzf --fish | source
 eval zoxide init fish | source
@@ -42,8 +40,8 @@ export FZF_ALT_C_COMMAND='fd -H -E .git -t d . $dir'
 export FZF_CTRL_T_COMMAND='fd -H -E .git -t f . $dir'
 export FZF_DEFAULT_COMMAND='fd -H -E .git'
 export FZF_ALT_C_OPTS='--preview "eza -T -L3 {} | head -100"'
-export FZF_CTRL_T_OPTS='--preview "bat -f -r :100 {}"'
-export FZF_DEFAULT_OPTS='-0 --multi --tmux 85% --border --style=full --info=inline-right --marker ▏ --pointer ▌ --highlight-line --color marker:green,pointer:green,prompt:green,gutter:-1,selected-bg:238'
+export FZF_CTRL_T_OPTS='--preview "bat -f -r :100 {}" --bind "focus:bg-transform-header(file -bI {})"'
+export FZF_DEFAULT_OPTS='-0 --multi --tmux 85% --border --style=full --info=inline-right --marker ▏ --pointer ▌ --highlight-line --color marker:green,pointer:green,prompt:green,gutter:-1,selected-bg:238,border:#9999cc'
 alias f='fzf'
 
 export EZA_COLORS='da=2;0:gm=1;0'
