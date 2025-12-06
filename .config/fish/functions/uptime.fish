bef506387df28b6196abab2085d20fd8cf5c96df
XhstormR <10527522+XhstormR@users.noreply.github.com>
2025-12-06 04:17:31 +0300

    Update

---
+++ .config/fish/functions/uptime.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+function uptime
+    set -l diff (expr (date +%s) - (date +%s -d (command uptime -s)))
+    set -l d (math --scale 0 $diff / 86400); set diff (math $diff % 86400)
+    set -l h (math --scale 0 $diff /  3600); set diff (math $diff %  3600)
+    set -l m (math --scale 0 $diff /    60); set diff (math $diff %    60)
+    printf "%02dd %02dh %02dm\n" $d $h $m
+end
